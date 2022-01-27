5e5f794e7d3d9959d9013002864c3572c6d06aae
BIRMCAM <43913293+birmcam@users.noreply.github.com>
2022-01-27 10:04:20 +0000

    Added Linda's files

---
+++ cluster_method_hca.R
@@ -0,0 +1,42 @@
+#
+#
+### Multimorbidity project
+#
+#
+#
+#
+#  Hierarchical cluster analysis
+#
+#
+#
+
+
+#rv1 <- make_fake_data( K = 3, allow_overlap = TRUE, balanced = TRUE, N_PATIENTS = 3000, N_NULL = 100, NOISE_PARAMETER = -Inf, MEAN_VAL = 2.0, COR_VAL = 0.9)
+
+
+#dat2 <- rv1$data
+
+
+### HCA.  Distance measure: binary
+
+run_hca <- function(df, Kval)  {
+  
+  dis_ind_bin <- dist(df[ , 2:27], method="binary", by_rows=TRUE)
+
+#  hca.indb.c <- hclust(dis_ind_bin, method = "complete")
+#  hca.indb.a <- hclust(dis_ind_bin, method = "average")
+  hca.indb.w <- hclust(dis_ind_bin, method = "ward.D")
+
+  hca.indb.w.cut <- cutree(hca.indb.w, k = Kval + 1) # cut tree at Kval + 1 cluster
+  outdf <- cbind(df, "predclust" = hca.indb.w.cut)
+  
+  adjri <- adjustedRandIndex(outdf$group, outdf$predclust)
+  
+  #print(table(outdf$group, outdf$predclust))
+  #print(adjri)
+  
+  return(adjri)
+
+}
+
+
---
+++ cluster_method_lca.R
@@ -0,0 +1,49 @@
+#
+#
+### Multimorbidity project
+#
+#
+#
+#
+# LCA function
+#
+#
+#
+
+
+
+
+### LCA
+
+
+run_lca <- function(df, Kval, ...)  {
+  on.exit(sink(file=NULL))
+
+  # recode presence/absence of condition as 1 = absence, 2 = presence as poLCA package needs positive integer for category labels
+  varlist <- names(df)[2:27]
+  df[varlist] <- df[varlist] + 1
+
+  f <- with(df, cbind(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z) ~ 1) 
+
+  sink(tmpfile <- tempfile())
+  ## fit LCA with K + 1 class
+  lca_mm <- poLCA(f, df, nclass=(Kval + 1), ...)
+                  #maxiter=1000, nrep=25
+                  #tol=1e-5, na.rm=FALSE, 
+                  #verbose=FALSE, calc.se=FALSE) 
+
+  unlink(tmpfile)
+  
+  # add most appropriate class based on highest posterior probability of membership
+  outdf <- cbind(df, "predclust" = lca_mm$predclass)
+
+  adjri <- adjustedRandIndex(outdf$group, outdf$predclust)
+  
+  #print(table(outdf$group, outdf$predclust))
+  #print(adjri)
+  
+  return(adjri)
+  
+}
+
+
